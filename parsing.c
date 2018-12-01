8631e73b347750acc42ae747585d69b8f38e6d57
Rigel Lute <rigellute@gmail.com>
2018-12-01 15:58:41 +0000

    add recursive evaluation

--- parsing.c
+++ parsing.c
@@ -16,6 +16,52 @@ char *readline(char *prompt)
 
 void add_history(char *unused) {}
 
+long eval_op(long x, char *op, long y)
+{
+    if (strcmp(op, "+") == 0)
+    {
+        return x + y;
+    }
+    if (strcmp(op, "-") == 0)
+    {
+        return x - y;
+    }
+    if (strcmp(op, "*") == 0)
+    {
+        return x * y;
+    }
+    if (strcmp(op, "/") == 0)
+    {
+        return x / y;
+    }
+
+    return 0;
+}
+
+long eval(mpc_ast_t *t)
+{
+    // If tagged as number return it directly
+    if (strstr(t->tag, "number"))
+    {
+        return atoi(t->contents);
+    }
+
+    // The operator is always second child.
+    char *op = t->children[1]->contents;
+
+    // Store the third child in `x`
+    long x = eval(t->children[2]);
+
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr"))
+    {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 #else
 #include <editline/readline.h>
 #include <editline/history.h>
@@ -53,8 +99,8 @@ int main(int argc, char **argv)
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r))
         {
-            /* On success print and delete the AST */
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         }
         else
