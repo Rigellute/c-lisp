08c1370a7640f69cd2421f1352eb943c3f4e2d4c
Rigel Lute <rigellute@gmail.com>
2018-12-01 11:58:26 +0000

    make prompt portable

--- prompt.c
+++ prompt.c
@@ -1,26 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a bugger for user input of size 2048 */
-static char input[2048];
+// If we are compiling on Windows compile these functions
+#ifdef _WIN32
+#include <string.h>
+
+// Declare a buffer for user input of size 2048
+static char buffer[2048];
+
+// Make a fake readline for windows portability
+char *readline(char *prompt)
+{
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char *cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+    return cpy;
+}
+
+// Fade add_history for windows
+void add_history(char *unused) {}
+
+// Otherwise include the editline headers
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
 
 int main(int argc, char **argv)
 {
 
-    /* Print Version and Exit Information */
+    // Print Version and Exit Information
     puts("Lispy Verison 0.0.1");
     puts("Press Ctrl+c to Exit\n");
 
     while (1)
     {
-
-        // Output for prompt
-        fputs("lispy> ", stdout);
-
-        // Read a line of user input of maximum size 2048
-        fgets(input, 2048, stdin);
+        char *input = readline("lispy> ");
+        add_history(input);
 
         // Echo input back to user
-        printf("No you're a %s", input);
+        printf("No you're a %s\n", input);
+
+        free(input);
     }
 
     return 0;
